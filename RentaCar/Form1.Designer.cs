8ec831414c816845f1dffed54022692b6f754685
Néstor Martínez Hernandez <nestormartinezh@outlook.com>
2017-08-01 14:19:44 -0500

    inicializar

--- RentaCar/Form1.Designer.cs
+++ RentaCar/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RentaCar
 {
-    partial class Form1
+    partial class RentaCar
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // RentaCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "RentaCar";
+            this.Text = "RentaCar";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- RentaCar/Form1.cs
+++ RentaCar/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RentaCar
 {
-    public partial class Form1 : Form
+    public partial class RentaCar : Form
     {
-        public Form1()
+        public RentaCar()
         {
             InitializeComponent();
         }
